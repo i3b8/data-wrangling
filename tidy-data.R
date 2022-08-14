a98a495ad677bcd20306b38f8c8e463f8f5ceaf4
Ilyes Ben Ayedgit config --global user.email ilyes.benayed@epfl.ch <ilyes.benayed@epfl.ch>
2022-08-14 22:40:58 +0200

    Reshaping data: pivot_longer

--- tidy-data.R
+++ tidy-data.R
@@ -14,3 +14,18 @@ tidy_data %>% ggplot(aes(year,fertility,color=country))+
 path <- system.file("extdata",package="dslabs")
 filename <- file.path(path,"fertility-two-countries-example.csv")
 wide_data <- read_csv(filename)
+
+
+## Reshpae the data : pivot longer 
+wide_data %>% select(country,'1960':'1965')
+## move columns 61 ... to a single column 
+wide_data %>% pivot_longer('1960':'2015')
+## or ##
+wide_data%>% pivot_longer(-country)
+## give meaningful names for columns 
+new_tidy_data <- wide_data %>% pivot_longer(-country,names_to = "year", values_to = "fertility")
+## transform to integers 
+new_tidy_data <- wide_data %>% pivot_longer(-country,names_to = "year", values_to = "fertility",
+                                            names_transform = list(year=as.numeric))
+class(new_tidy_data$year)
+head(new_tidy_data)
