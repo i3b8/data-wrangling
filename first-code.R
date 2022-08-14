11eace836db5a14d4479b9ee30bc96274a1a101e
Ilyes Ben Ayedgit config --global user.email ilyes.benayed@epfl.ch <ilyes.benayed@epfl.ch>
2022-08-14 13:16:39 +0200

    Paths and the Working Directory

--- first-code.R
+++ first-code.R
@@ -1,4 +1,16 @@
 filename <- "murders.csv"
 dir <- system.file("extdata", package = "dslabs") 
 fullpath <- file.path(dir, filename)
-file.copy(fullpath, "murders.csv")ls
+file.copy(fullpath, "murders.csv")
+
+system.file("extdata",package="dslabs")
+
+path <-system.file("extdata",package ="dslabs")
+list.files(path)
+
+filename <-"murders.csv"
+fullpath <- file.path(path,filename)
+fullpath
+file.copy(fullpath,getwd())
+file.exists((filename))
+
